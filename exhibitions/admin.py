a801eb87966bd30d5a0eaabc3235d6bce41b41d8
manterolat <manterolat@users.noreply.github.com>
2018-07-12 14:28:37 -0400

    Add models and admin registration

--- exhibitions/admin.py
+++ exhibitions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Gallery)
+admin.site.register(Exhibition)
+admin.site.register(Staff)
+
+admin.site.register(Artifact)
+admin.site.register(Organism)
+admin.site.register(Fossil)
+admin.site.register(Artwork)
--- exhibitions/models.py
+++ exhibitions/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Gallery(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Galleries"
+
+class Exhibition(models.Model):
+    name = models.CharField(max_length=100)
+    sponsor = models.CharField(max_length=100)
+    start_date = models.DateField('Date exhibition starts')
+    end_date = models.DateField('Date exhibition ends')
+    active = models.BooleanField()
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField('Item description')
+    item_type = models.CharField(max_length=10)
+    exhibition = models.ForeignKey(Exhibition, on_delete=models.CASCADE)
+
+class Artifact(Item):
+    country_origin = models.CharField(max_length=100)
+    excavation_site = models.CharField(max_length=150)
+    discovery_date = models.DateField('Date artifact was discovered')
+    condition = models.CharField(max_length=50)
+    material = models.CharField(max_length=50)
+
+class Organism(Item):
+    scientific_name = models.CharField(max_length=100)
+    condition = models.CharField(max_length=100)
+
+class Fossil(Item):
+    country_origin = models.CharField(max_length=100)
+    excavation_site = models.CharField(max_length=150)
+    discovery_date = models.DateField('Date artifact was discovered')
+    period = models.CharField(max_length=50)
+
+class Artwork(Item):
+    art_type = models.CharField(max_length=50)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+class Staff(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=50)
+    shift = models.CharField('Day or Night shift', max_length=50)
+
+    works_in = models.ManyToManyField(Gallery)
+
+    class Meta:
+        verbose_name_plural = "Staff"
