1781bc2a0e79c00a607bccddeaf0d260022610a4
manterolat <manterolat@users.noreply.github.com>
2018-07-21 16:29:38 -0400

    Add suggestion to items and fix search artwork view

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -8,21 +8,110 @@ def index(request):
     return render(request, 'exhibitions/index.html', {})
 
 def get_item(request, item_id):
-    if Item.objects.filter(pk=item_id).count():
+    if Item.objects.filter(pk=item_id).exists():
         item = Item.objects.get(pk=item_id)
     else:
         raise Http404("Item not found")
 
+    def name_similarity(name1, name2):
+        if name1 in name2 or name2 in name1:
+            return 0
+
+        diff = abs(len(name2) - len(name1))
+        for i in range(min(len(name1), len(name2))):
+            if name1.lower()[i] != name2.lower()[i]:
+                diff += 1
+        
+        return diff
+
+    suggested = []
+
     if item.artifact:
         item = item.artifact
+        suggested = list(map(list, zip([0 for i in range(Artifact.objects.all().count())], Artifact.objects.all().exclude(pk__exact=item.pk))))
+
+        for i in range(len(suggested)):
+            item2 = suggested[i][1]
+
+            name_score = name_similarity(item.name, item2.name)
+            exhibition_score = 1 if item.exhibition.pk != item2.exhibition.pk else 0
+            
+            origin_score = 1 if item.country_origin != item2.country_origin else 0
+            excavation_score = 1 if item.excavation_site != item2.excavation_site else 0
+            year_score = abs(item.discovery_year - item2.discovery_year)
+            condition_score = 1 if item.condition != item2.condition else 0
+            material_score = 1 if item.material != item2.material else 0
+
+            total_score = name_score + (exhibition_score * 2)
+            total_score += (origin_score * 5) + (year_score // 2)
+            total_score += (condition_score * 3) + (material_score * 2)
+            suggested[i][0] = total_score
+
+        suggested = list(sorted(suggested, key=lambda tup: tup[0]))[:10]
+
     elif item.organism:
         item = item.organism
+        suggested = list(map(list, zip([0 for i in range(Organism.objects.all().count())], Organism.objects.all().exclude(pk__exact=item.pk))))
+
+        for i in range(len(suggested)):
+            item2 = suggested[i][1]
+
+            name_score = name_similarity(item.name, item2.name)
+            exhibition_score = 1 if item.exhibition.pk != item2.exhibition.pk else 0
+            
+            sci_name_score = 1 if item.sci_name_score != item2.sci_name_score else 0
+            condition_score = 1 if item.condition != item2.condition else 0
+
+            total_score = name_score + (exhibition_score * 2)
+            total_score += sci_name_score + (condition_score * 2)
+            suggested[i][0] = total_score
+
+        suggested = list(sorted(suggested, key=lambda tup: tup[0]))[:10]
+
     elif item.fossil:
         item = item.fossil
-    elif item.artword:
+        suggested = list(map(list, zip([0 for i in range(Fossil.objects.all().count())], Fossil.objects.all().exclude(pk__exact=item.pk))))
+
+        for i in range(len(suggested)):
+            item2 = suggested[i][1]
+
+            name_score = name_similarity(item.name, item2.name)
+            exhibition_score = 1 if item.exhibition.pk != item2.exhibition.pk else 0
+            
+            origin_score = 1 if item.country_origin != item2.country_origin else 0
+            excavation_score = 1 if item.excavation_site != item2.excavation_site else 0
+            year_score = abs(item.discovery_year - item2.discovery_year)
+            period_score = 1 if item.period != item2.period else 0
+
+            total_score = name_score + (exhibition_score * 2)
+            total_score += (origin_score * 5) + (year_score // 2)
+            total_score += (period_score * 10)
+            suggested[i][0] = total_score
+
+        suggested = list(sorted(suggested, key=lambda tup: tup[0]))[:10]
+
+    elif item.artwork:
         item = item.artwork
+        suggested = list(map(list, zip([0 for i in range(Artwork.objects.all().count())], Artwork.objects.all().exclude(pk__exact=item.pk))))
+
+        for i in range(len(suggested)):
+            item2 = suggested[i][1]
+
+            name_score = name_similarity(item.name, item2.name)
+            exhibition_score = 1 if item.exhibition.pk != item2.exhibition.pk else 0
+            
+            type_score = 1 if item.art_type != item2.art_type else 0
+            author_score = 1 if item.author != item2.author else 0
+            year_score = abs(item.year - item2.year)
+
+            total_score = name_score + (exhibition_score * 2)
+            total_score += (type_score * 5) + (year_score // 2)
+            total_score += (author_score * 5)
+            suggested[i][0] = total_score
+
+        suggested = list(sorted(suggested, key=lambda tup: tup[0]))[:10]
     
-    return render(request, "exhibitions/item.html", {"item": item, "type": item.__class__.__name__})
+    return render(request, "exhibitions/item.html", {"item": item, "type": item.__class__.__name__, "suggestions": suggested})
 
 def search_artifacts(request):
     if request.method == 'POST':
